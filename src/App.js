711c27954da9f4a73856f07f2fca47b2fa0e0994
leti <meteorite@gmail.com>
2020-01-31 15:45:26 +0100

    merge incoming

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
+import PagePresentationProjet from "./components/FormulaireCreationProjet/PagePresentationProjet";
 import FormulaireCreationProjet from "./components/FormulaireCreationProjet/FormulaireCreationProjet";
 
 function App() {
   return (
     <div className="App">
-      <FormulaireCreationProjet />
+      <PagePresentationProjet />
+      {/* <FormulaireCreationProjet /> */}
     </div>
   );
 }
