b1836c4e9f950ba7094a855d244a2ebcbf418727
BaudhuinPie <baudhuin_pierre.pro@mailo.com>
2020-01-31 15:43:20 +0100

    sous-categories icones ajoutees

--- src/components/config/objectifs.js
+++ src/components/config/objectifs.js
@@ -1,9 +1,16 @@
+import React from "react";
+import logo1 from "./../Logos/sdg-01-picto-fr.png";
+import logo2 from "./../Logos/sdg-02-picto-fr.jpg";
+import logo3 from "./../Logos/sdg-03-picto-fr.jpg";
+import logo4 from "./../Logos/sdg-04-picto-fr.jpg";
+
+
 const objectifs = [
   "init",
-  "Pas de pauvreté",
-  'Faim "zéro"',
-  "Bonne santé & Bien-Être",
-  "Education de qualité",
+  [<img src={logo1} alt="Faim zéro" width="150px" height="Pas de pauvreté"/>],
+  [<img src={logo2} alt="Faim zéro" width="150px" height="auto"/>],
+  [<img src={logo3} alt="Faim zéro" width="150px" height="Bonne santé & Bien-Être"/>],
+  [<img src={logo4} alt="Faim zéro" width="150px" height="Education de qualité"/>],
   "Egalité des sexes",
   "Eau propre & Assainissement",
   "Energie propre & abordable",
